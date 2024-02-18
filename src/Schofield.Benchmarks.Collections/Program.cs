fd28bb0bcce90cea530105ed7072ee5d106f1871
Chris Schofield <5215009+christopher-schofield@users.noreply.github.com>
2024-02-17 22:33:55 -0700

    Clean up a bit for comments

--- src/Schofield.Benchmarks.Collections/Program.cs
+++ src/Schofield.Benchmarks.Collections/Program.cs
@@ -1,17 +1,14 @@
 ﻿using Schofield.Benchmarks.Collections.Tests;
 
 
-
 new ObjectBenchmark(100000, 10000).Execute();
 new ObjectBenchmark(1000000, 25000).Execute();
-//This takes a while
-new ObjectBenchmark(10000000, 50000).Execute();
+new ObjectBenchmark(10000000, 50000).Execute(); // This takes a while
 
 new IntegerBenchmark(1000000, 10000, 100000000).Execute();
 new IntegerBenchmark(10000000, 25000, 100000000).Execute();
-new IntegerBenchmark(100000000, 50000, 100000000).Execute();
+new IntegerBenchmark(100000000, 50000, 100000000).Execute(); // This takes a while
 
 new StringBenchmark(100000, 10000).Execute();
 new StringBenchmark(1000000, 25000).Execute();
-//This takes a while
-new StringBenchmark(10000000, 50000).Execute();
+new StringBenchmark(10000000, 50000).Execute(); // This takes a while
